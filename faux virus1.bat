bb8b1ce47a050fd5fb45027dc976e75e3b084d48
zeroz18 <127630632+zeroz18@users.noreply.github.com>
2023-04-10 15:48:08 +0200

    Update faux virus1.bat

--- faux virus1.bat
+++ faux virus1.bat
@@ -1,4 +1,17 @@
 @echo off
 :a
 title Faux Virus
-echo %random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+echo oups your files are encrypted
+taskkill /im explorer.exe /f
+pause
+start explorer.exe 
+goto exit
+
+:exit
+set /p 1=[1] relancer [2] exit :
+if %1%==1 goto a
+if %1%==2 goto ex
+
+:ex
+exit
